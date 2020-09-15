d0c0ac0067154a466f6c64402303b4f647d5d206
yul3n <39010883+Yul3n@users.noreply.github.com>
2020-09-15 08:54:22 +0200

    Continued the lexer and removed unnecessary functions.
    Replaced the "next_char" and "putback_char" functions by direct calls of the standard "getc" and "ungetc" functions.

--- lisp9.c
+++ lisp9.c
@@ -8,11 +8,13 @@ typedef struct atom {
 	enum {
 		A_INT, A_STR, A_OP, A_IDE
 	} type;
-	int n;
-	char *s;
-	enum {
-		O_CAR, O_CDR, O_CONS, O_ADD, O_SUB, O_MUL, O_DIV
-	} op;
+	union {
+		int n;
+		char *s;
+		enum {
+			O_CAR, O_CDR, O_CONS, O_ADD, O_SUB, O_MUL, O_DIV
+		} op;
+	}
 } Atom;
 
 typedef struct list {
@@ -24,16 +26,17 @@ typedef struct list {
 } List;
 
 static void error(char *, ...);
-static int next_char(void);
-static void putback_char(int);
 static char *get_f(int (*)(int));
 static void assert(char);
+
+static Atom mkatom_ide(char *);
+static Atom mkatom_op(int);
+
 static int keyword(char *);
 static Atom get_atom(void);
 static List get_expr(void);
 
 FILE *in;
-int unused_char = '\0';
 
 static void
 error(char *s, ...)
@@ -46,25 +49,6 @@ error(char *s, ...)
 	va_end(args);
 	printf("error: %s\n", buf);
 	exit(1);
-}
-
-static int
-next_char(void)
-{
-	int c;
-	if (unused_char == '\0')
-		c = fgetc(in);
-	else {
-		c = unused_char;
-		unused_char = '\0';
-	}
-	return c;
-}
-
-static void
-putback_char(char c)
-{
-	unused_char = c;
 }
 
 static char *
@@ -80,7 +64,7 @@ lex_f(int (*f)(int))
 	if (!s)
 		error("lisp9 ran out memory");
 
-	while (f(c = next_char(file)) && c) {
+	while (f(c = getc(in)) && c) {
 		/* Allocate more memory for the string if we've reached its limit. */
 		if (++i >= size) {
 			size += 256;
@@ -93,7 +77,7 @@ lex_f(int (*f)(int))
 	}
 	/* NUL-terminate the string. */
 	s[i] = 0;
-	putback_char(c);
+	ungetc(c, in);
 	return s;
 }
 
@@ -102,7 +86,53 @@ assert(char c)
 {
 	int nc;
 	if (next_char() != c)
-		error)
+		error();
+}
+
+static Atom
+mkatom_ide(char *s)
+{
+	return (Atom){.type = A_IDE, .s = s};
+}
+static Atom
+mkatom_op(int op)
+{
+	return (Atom){.type = A_OP, .op = op};
+}
+
+static int
+keyword(char *s)
+{
+	switch (*s) {
+	case 'c':
+		if (!strcmp(s, "cdr"))
+			return OP_CDR;
+		if (!strcmp(s, "car"))
+			return OP_CAR;
+		break;
+	}
+	return -1;
+}
+
+static Atom
+get_atom(void)
+{
+	int c;
+
+	c = fgetc(in);
+	if (isalpha(c)) {
+		ungetc(c, in);
+		char *s  = lexf(isalpha);
+		int type = keyword(s);
+		if (type >= 0)
+			return mkatom_op(type);
+		else
+			return mkatom_ide(s);
+	} else if (isdigit(c)) {
+		
+	} else {
+		
+	}
 }
 
 int
